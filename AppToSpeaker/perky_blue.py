3ab6e3acdec2f0c68032d6750bfad5e38213f64f
Trey Lewis <lewistd@rose-hulman.edu>
2018-11-01 18:21:08 +0000

    added in a separate folder for the final files

---
+++ AppToSpeaker/perky_blue.py
@@ -0,0 +1,110 @@
+#! /usr/bin/python
+#
+# Title:perky_blue.py
+# Description:
+# Development Environment:OS X 10.9.3/Python 2.7.7
+# Author:G.S. Cole (guycole at gmail dot com)
+#
+import gpio
+import time
+import subprocess
+import shlex
+
+from mplayer import *
+
+from bluetooth import *
+
+class PerkyBlueServer:
+
+    def playSong(self, target):
+	if target == 'Kalimba':
+		audiofile = target + '.mp3'
+		commandline = 'mplayer -slave -quiet -ao alsa:device=default=Set %s' % audiofile
+		proc = subprocess.Popen(shlex.split(commandline), stdin=PIPE)
+
+		time.sleep(10)
+		#pause the music
+		proc.stdin.write('pause\n')
+		time.sleep(10)
+		#unpause the music
+		proc.stdin.write('pause\n')
+		time.sleep(10)
+		#stop the music
+		proc.stdin.write('stop\n')
+
+	elif:
+		print 'unknown remote command'
+
+
+
+
+
+
+
+    def toggleLed(self, target):
+	if target == 'green':
+		print 'toggle green'
+		self.green_led.toggleGpioValue()
+	elif target == 'red':
+		print 'toggle red'
+		self.red_led.toggleGpioValue()
+#        elif target == 'yellow':
+#		print 'toggle yellow'
+#		self.yellow_led.toggleGpioValue()
+	else:
+		print 'unknown remote command'
+
+    def execute(self):
+	self.green_led = gpio.gpio(48)
+        self.green_led.setDirectionValue('out')
+        self.green_led.setGpioValue(0)
+
+	self.red_led = gpio.gpio(49)
+        self.red_led.setDirectionValue('out')
+        self.red_led.setGpioValue(0)
+
+#	self.yellow_led = gpio.gpio(60)
+#        self.yellow_led.setDirectionValue('out')
+#        self.yellow_led.setGpioValue(0)
+
+        service_uuid = "00001101-0000-1000-8000-00805F9B34FB"
+
+        server_sock = BluetoothSocket(RFCOMM)
+        server_sock.bind(("", PORT_ANY))
+        server_sock.listen(1)
+
+        port = server_sock.getsockname()[1]
+
+        advertise_service(server_sock, "PerkyBlue", service_id = service_uuid, service_classes = [service_uuid, SERIAL_PORT_CLASS], profiles = [SERIAL_PORT_PROFILE])
+
+        print("awaiting RFCOMM connection on channel:%d" % port)
+
+        client_sock, client_info = server_sock.accept()
+        print("accepted connection from:", client_info)
+
+        try:
+            while True:
+                data = client_sock.recv(1024).strip()
+                if len(data) == 0: break
+                print("received [%s]" % data)
+                client_sock.sendall('OK')
+
+                #self.toggleLed(data)
+		self.playSong(data)
+        except IOError:
+            pass
+
+        print("disconnected")
+
+        client_sock.close()
+        server_sock.close()
+        print("all done")
+
+print 'start'
+
+#
+if __name__ == '__main__':
+    server = PerkyBlueServer()
+    server.execute()
+
+print 'stop'
