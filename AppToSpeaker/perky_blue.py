b3764bee5fc5dc23a4de6392bc55ccf1f08e3241
Trey Lewis <lewistd@rose-hulman.edu>
2018-11-05 04:45:08 +0000

    made some good progress, fixed audio output issues

--- AppToSpeaker/perky_blue.py
+++ AppToSpeaker/perky_blue.py
@@ -5,6 +5,7 @@ import time
 import subprocess
 import shlex
 import os
+import signal
 
 from bluetooth import *
 
@@ -13,7 +14,7 @@ class MusicAppServer:
     def playSong(self, target, client_sock):
 	if target == 'Kalimba':
 		audiofile = target + '.mp3'
-		commandline = 'mplayer -ac mad -slave -quiet -ao alsa:device=default=Set %s' % audiofile
+		commandline = 'mplayer -af resample=48000:0:2 -slave -quiet -ao alsa:device=default=Set %s' % audiofile
 		proc = subprocess.Popen(shlex.split(commandline), stdin=subprocess.PIPE)
 		running = 1
 		paused = 0
@@ -39,7 +40,7 @@ class MusicAppServer:
 
 	elif target == 'GoodLife':
 		audiofile = target + '.mp3'
-		commandline = 'mplayer -speed 0.75 -cache 8192 -cache-min 99 -slave -quiet -ao alsa:device=default=Set %s' % audiofile
+		commandline = 'mplayer -af resample=48000:0:2 -slave -quiet -ao alsa:device=default=Set %s' % audiofile
                 proc = subprocess.Popen(shlex.split(commandline), stdin=subprocess.PIPE)
 		running = 1
 		paused = 0
@@ -62,6 +63,32 @@ class MusicAppServer:
 				proc.stdin.write('pause\n')
 				paused = 0
 
+	elif target == 'Nocturne':
+                audiofile = target + '.mp3'
+                commandline = 'mplayer -af resample=48000:0:2 -slave -quiet -ao alsa:device=default=Set %s' % audiofile
+                proc = subprocess.Popen(shlex.split(commandline), stdin=subprocess.PIPE)
+                running = 1
+                paused = 0
+
+                while (running):
+                        data = client_sock.recv(1024).strip()
+                        if len(data) == 0: break
+                        print("received [%s]" % data)
+                        client_sock.sendall('OK')
+
+                        if (data == 'stop'):
+                                proc.stdin.write('stop\n')
+                                running = 0
+
+                        elif ((data == 'pause') and (paused == 0)):
+                                proc.stdin.write('pause\n')
+                                paused = 1
+
+                        elif ((data == 'play') and (paused == 1)):
+                                proc.stdin.write('pause\n')
+                                paused = 0
+
+
 	elif ((target == 'pause') or (target == 'stop')):
 		print 'A song must be playing'
 
@@ -73,8 +100,17 @@ class MusicAppServer:
 
 
     def execute(self):
+		# try to automatically make device bluetooth discoverable
+#		os.system('bluetoothctl')
 
-	os.system('bluetoothctl')
+#	pid, fd = os.forkpty()
+
+#	if pid:
+
+#		os.write(fd, 'bluetoothctl')
+#		os.write(fd, 'discoverable yes')
+#		os.write(fd, 'quit')
+#		os.kill(pid, signal.SIGKILL)
 
         service_uuid = "00001101-0000-1000-8000-00805F9B34FB"
 
@@ -84,7 +120,7 @@ class MusicAppServer:
 
         port = server_sock.getsockname()[1]
 
-        advertise_service(server_sock, "PerkyBlue", service_id = service_uuid, service_classes = [service_uuid, SERIAL_PORT_CLASS], profiles = [SERIAL_PORT_PROFILE])
+        advertise_service(server_sock, "MusicApp", service_id = service_uuid, service_classes = [service_uuid, SERIAL_PORT_CLASS], profiles = [SERIAL_PORT_PROFILE])
 
         print("awaiting RFCOMM connection on channel:%d" % port)
 
