493c43cdcbafe3a518f743e824b79207addd0222
Trey Lewis <lewistd@rose-hulman.edu>
2018-11-08 18:52:46 +0000

    pushing all my updates

--- AppToSpeaker/perky_blue.py
+++ AppToSpeaker/perky_blue.py
@@ -1,6 +1,5 @@
 #! /usr/bin/python
 
-import gpio
 import time
 import subprocess
 import shlex
@@ -96,21 +95,39 @@ class MusicAppServer:
 		print 'Select a song to play'
 
 	else:
-		print 'unknown remote command'
+		print 'attempting to download or play file: %s' % target
+		audiofile = target + '.mp3'
+		if (not os.path.isfile(audiofile)):
+			os.system('wget %s' %target)
+		if (os.path.isfile(audiofile)):
+			commandline = 'mplayer -af resample=48000:0:2 -slave -quiet -ao alsa:device=default=Set %s' % audiofile
+	                proc = subprocess.Popen(shlex.split(commandline), stdin=subprocess.PIPE)
+        	        running = 1
+              		paused = 0
+
+	                while (running):
+	                        data = client_sock.recv(1024).strip()
+        	                if len(data) == 0: break
+                        	print("received [%s]" % data)
+                        	client_sock.sendall('OK')
+
+                        	if (data == 'stop'):
+                                	proc.stdin.write('stop\n')
+                                	running = 0
+
+                        	elif ((data == 'pause') and (paused == 0)):
+                                	proc.stdin.write('pause\n')
+                                	paused = 1
+
+                        	elif ((data == 'play') and (paused == 1)):
+                                	proc.stdin.write('pause\n')
+                                	paused = 0
+		else:
+			print 'ready for next command'
 
 
     def execute(self):
-		# try to automatically make device bluetooth discoverable
-#		os.system('bluetoothctl')
-
-#	pid, fd = os.forkpty()
-
-#	if pid:
-
-#		os.write(fd, 'bluetoothctl')
-#		os.write(fd, 'discoverable yes')
-#		os.write(fd, 'quit')
-#		os.kill(pid, signal.SIGKILL)
+	# try to automatically make device bluetooth discoverable
 
         service_uuid = "00001101-0000-1000-8000-00805F9B34FB"
 
